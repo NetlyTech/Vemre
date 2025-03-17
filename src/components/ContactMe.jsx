991e62a42b7527a0c7752ce1eaa87a329d1e1a14
NetlyTech <Netly@one1>
2025-03-17 11:42:36 +0000

    Testing gh-paages

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -17,6 +17,8 @@ const ContactMe = () => {
       </div>
       <div className="md:w-1/2 p-4">
         <h2 className="text-2xl font-bold mb-4 text-[#ffffff]">Find us via the Map</h2>
+
+        {/* <div></div> */}
        
 
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3321.944801469005!2d-83.98830380986955!3d33.63267319786009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f44b3c65996e4b%3A0xe3f5ab6f33ffb207!2s3826%20Salem%20Rd%20%23194%2C%20Covington%2C%20GA%2030016%2C%20USA!5e0!3m2!1sen!2sng!4v1741594933492!5m2!1sen!2sng" width="100%" height="300" style={{ border: 0 }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
