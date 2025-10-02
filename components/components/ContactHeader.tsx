4c24311775feb1b8616aa470740dbdfd4cd74290
Netly Tech <netlysolutions@gmail.com>
2025-10-02 20:19:07 +0100

    edited contact

--- components/components/ContactHeader.tsx
+++ components/components/ContactHeader.tsx
@@ -9,7 +9,7 @@ import React from 'react';
       <div className="flex items-center rounded-b-sm shadow-lg justify-center mb-8 w-full">
        
        <div>
-         <h4 className="text-green-800 text-center text-2xl md:text-4xl font-extrabold mb-2">Get In Touch</h4>
+         <h4 className="text-green-800 text-center text-3xl md:text-4xl font-extrabold mb-2">Get In Touch</h4>
         <h1 className="text-center text-[12px] md:text-[16px] lg:text-[20px] font-bold mb-4">
           Visit Us on Site or Contact Us Today!
         </h1>
